8d54de0558105611206abce328d61a148a8bf1ea
Adam Mackintosh <agm1984@gmail.com>
2017-08-15 01:04:58 -0700

    added stuff

--- app.js
+++ app.js
@@ -1,58 +1,59 @@
-var express = require('express');
-var app = express();
-var request = require('request');
-var bodyParser = require('body-parser');
+'use strict'
+const express = require('express')
+const app = express()
+const request = require('request')
+const bodyParser = require('body-parser')
 
 // Load Sentiment Analysis Library
-const sentiment = require('sentiment');
+const sentiment = require('sentiment')
 
 // Load Word Frequency Counter Library
-const freq = require('freq');
+const freq = require('freq')
 
 // Set public assets directory and templating engine
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
 // Main Route
 app.get('/', (req, res) => {
-  res.render('search');
-});
+    res.render('search')
+})
 
 // Results Route
 app.get('/results', (req, res) => {
-  // Grab value from search.ejs form input with name='search'
-  const search = req.query.search;
+    // Grab value from search.ejs form input with name='search'
+    const search = req.query.search
 
-  // Run Sentiment Analysis
-  const sentimentResults = sentiment(search);
+    // Run Sentiment Analysis
+    const sentimentResults = sentiment(search)
 
-  // Run Word Frequency Counter
-  const freqResults = freq(sentimentResults.words);
+    // Run Word Frequency Counter
+    const freqResults = freq(sentimentResults.words)
 
-  // Prepare object for Front-end
-  const data = {
-    sentiment: {
-      score: sentimentResults.score,
-      comparative: sentimentResults.comparative,
-      tokens: sentimentResults.tokens,
-      words: sentimentResults.words,
-      positive: sentimentResults.positive,
-      negative: sentimentResults.negative,
-    },
-    freq: {
-      results: freqResults
+    // Prepare object for Front-end
+    const data = {
+        sentiment: {
+        score: sentimentResults.score,
+        comparative: sentimentResults.comparative,
+        tokens: sentimentResults.tokens,
+        words: sentimentResults.words,
+        positive: sentimentResults.positive,
+        negative: sentimentResults.negative
+        },
+            freq: {
+            results: freqResults
+        }
     }
-  };
 
- res.render('results', { data: data });
-});
+    res.render('results', { data: data })
+})
 
 // Server Config
-var config = {
-  port: 1337,
-};
+const config = {
+    port: 1337,
+}
 
 // Start Server
 app.listen(config.port, () => {
     console.log("Server started on port: " + config.port);
-});
+})
--- public/results.js
+++ public/results.js
@@ -1,15 +1,17 @@
-var status = "hidden";
+// Set initial state
+let status = 'hidden'
 
 function toggleText() {
-    // var text="Here is some text that I want added to the HTML file";
 
-    if (status == "hidden") {
-        document.getElementById("textArea").style.display = "initial";
-        document.getElementById("toggleButton").innerText = "Hide Text";
-        status = "visible";
-    } else if (status == "visible") {
-        document.getElementById("textArea").style.display = "none";
-        document.getElementById("toggleButton").innerText = "See Text";
-        status = "hidden";
+    if (status == 'hidden') {
+
+        document.getElementById('textArea').style.display = 'initial'
+        document.getElementById('toggleButton').innerText = 'Hide Text'
+        status = 'visible'
+    } else if (status == 'visible') {
+
+        document.getElementById('textArea').style.display = 'none'
+        document.getElementById('toggleButton').innerText = 'See Text'
+        status = 'hidden'
     }
 }
